98754a7fba8d27e2406ae1a8f58b7b095abc5edd
Alex Deaconu <alex.deaconu@gmail.com>
2015-12-28 10:22:56 -0500

    Redirect to home page
    When a user is not signed up or is new
    redirect them to the home page when they click
    the app launcher buttons
    
    [stage-2]

--- web/scripts/schedules/services/svc-access.js
+++ web/scripts/schedules/services/svc-access.js
@@ -1,18 +1,22 @@
 'use strict';
 
 angular.module('risevision.schedules.services')
-  .factory('canAccessSchedules', ['$q', 'userState',
-    function ($q, userState) {
+  .factory('canAccessSchedules', ['$q', 'userState', '$state',
+    function ($q, userState, $state) {
       return function () {
         var deferred = $q.defer();
         userState.authenticate(false).then(function () {
             if (userState.isRiseVisionUser()) {
               deferred.resolve();
             } else {
-              deferred.reject();
+              return $q.reject();
             }
           })
-          .then(null, deferred.reject);
+          .then(null, function() {
+            $state.go('apps.schedules.home');
+            
+            deferred.reject();
+          });
         return deferred.promise;
       };
     }
